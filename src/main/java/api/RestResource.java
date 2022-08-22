b675629ddf470e43702cd20a58dd78917661d410
kvishwakarma <kamal.vishwakarma@agileaxis.com>
2022-08-22 13:56:32 +0530

    commiting api methods RestResource.java

---
+++ src/main/java/api/RestResource.java
@@ -0,0 +1,23 @@
+package api;
+
+import io.restassured.response.Response;
+
+import static api.SpecBuilder.requestSpec;
+import static api.SpecBuilder.responseSpec;
+import static io.restassured.RestAssured.given;
+
+/**
+ * @author : Kamal
+ * @project : TestVagrant
+ * @created : 22/08/2022 - 11:51 AM
+ */
+
+public class RestResource {
+    public static Response get() {
+        return given().spec(requestSpec()).
+                when().get().
+                then().spec(responseSpec()).
+                extract().
+                response();
+    }
+}
