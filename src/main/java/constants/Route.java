91829a8251fbc235d79306fee41818b61553ce96
kvishwakarma <kamal.vishwakarma@agileaxis.com>
2022-08-22 13:51:23 +0530

    commiting api constants

---
+++ src/main/java/constants/Route.java
@@ -0,0 +1,12 @@
+package constants;
+
+/**
+ * @author : Kamal
+ * @project : TestVagrant
+ * @created : 22/08/2022 - 11:52 AM
+ */
+
+public class Route {
+    public static final String BASE_URI = "https://gist.github.com";
+    public static final String BASE_PATH = "/kumarpani/1e759f27ae302be92ad51ec09955e765/raw/184cef7125e6ef5a774e60de31479bb9b2884cb5/TeamRCB.json";
+}
