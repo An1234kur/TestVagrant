7f7f8f00f2d88ef88eec6fd52040d9f1d387b6ed
kvishwakarma <kamal.vishwakarma@agileaxis.com>
2022-08-22 14:18:48 +0530

    recommiting BaseClass

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -14,23 +14,28 @@ import java.lang.reflect.Method;
  * @created : 22/08/2022 - 11:49 AM
  */
 
+//  A base class which contain methods which execute as prerequisite to all class and methods
 public class BaseClass {
+    //Execute before the suite
     @BeforeSuite
     public void beforeSuite(ITestContext context) {
         System.out.println("==================== START OF SUITE: " + context.getCurrentXmlTest().getSuite().getName() + " ====================\n");
     }
 
+    //Execute before the @Test annotation
     @BeforeMethod
     public void beforeMethod(Method m) {
         System.out.println("==================== STARTING TEST: " + m.getName().toUpperCase() + " ====================");
         System.out.println("THREAD ID: " + Thread.currentThread().getId());
     }
 
+    //Execute after the @Test annotation
     @AfterMethod
     public void afterMethod(Method m) {
         System.out.println("==================== END OF TEST: " + m.getName().toUpperCase() + " ====================\n");
     }
 
+    //Execute After the suite
     @AfterSuite
     public void afterSuite(ITestContext context) {
         System.out.println("==================== End OF SUITE: " + context.getCurrentXmlTest().getSuite().getName() + " ====================");
