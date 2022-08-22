e6bf20d8690b742e68c3dadbed1f1260b8d2370d
kvishwakarma <kamal.vishwakarma@agileaxis.com>
2022-08-22 14:19:26 +0530

    recommiting Route class

--- src/main/java/constants/Route.java
+++ src/main/java/constants/Route.java
@@ -6,6 +6,7 @@ package constants;
  * @created : 22/08/2022 - 11:52 AM
  */
 
+//API constants
 public class Route {
     public static final String BASE_URI = "https://gist.github.com";
     public static final String BASE_PATH = "/kumarpani/1e759f27ae302be92ad51ec09955e765/raw/184cef7125e6ef5a774e60de31479bb9b2884cb5/TeamRCB.json";
